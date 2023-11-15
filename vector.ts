b042bcb13e47f257db466ebb92b7f00eb38ca840
Ankit Pasi <ankitpasi1@gmail.com>
2023-11-15 14:39:32 +0530

    [feat] listen on 0.0.0.0

--- vector.ts
+++ vector.ts
@@ -66,6 +66,6 @@ app.get("/", async (req: Request, res: Response) => {
 app.use("/items", itemRouter);
 app.use("/query", queryRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
